b6a72fcab888170ddd9e46432c8e686d87ed0fff
puanabidah <puanabidah01@gmail.com>
2022-04-19 22:36:28 +0700

    new commit

---
+++ TugasPertemuan7/Kasir/Kasir/MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: LENOVO
+ * Date: 01/04/2022
+ * Time: 7:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Kasir
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 376);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(832, 233);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(38, 28);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(197, 42);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Kode Barang";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(38, 80);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(197, 39);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Nama Barang";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(38, 136);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(148, 38);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Harga Beli";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(38, 188);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(157, 30);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Harga Jual";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(38, 243);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 31);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Jumlah";
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(38, 298);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(112, 32);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Satuan";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(263, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(321, 37);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(263, 77);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(321, 37);
+			this.textBox2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(263, 133);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(321, 37);
+			this.textBox3.TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(263, 185);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(321, 37);
+			this.textBox4.TabIndex = 4;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(263, 240);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(321, 37);
+			this.textBox5.TabIndex = 5;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox6.Location = new System.Drawing.Point(263, 295);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(321, 37);
+			this.textBox6.TabIndex = 6;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Tw Cen MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(637, 290);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(122, 46);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Simpan";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(858, 637);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "MainForm";
+			this.Text = "Kasir";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
---
+++ TugasPertemuan8/Kasir/Kasir/MainForm.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: LENOVO
+ * Date: 19/04/2022
+ * Time: 21:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+//tambahkan ini 
+using System.Data;
+using MySql.Data;
+using System.Data.OleDb;
+using MySql.Data.MySqlClient;
+
+namespace Kasir
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		MySqlConnection co = new MySqlConnection("Server = localhost; Database = kasir; Uid = root;");
+		MySqlCommand mycommand = new MySqlCommand();
+		MySqlDataAdapter myadapter = new MySqlDataAdapter();
+		
+		
+		public void TampilBarang(){
+			try{
+				mycommand.Connection = co;
+				myadapter.SelectCommand = mycommand;
+				mycommand.CommandText = "select * from tbl_barang";
+				DataSet ds = new DataSet();
+
+				if (myadapter.Fill(ds,"tbl_barang") > 0){
+					dataGridView1.DataSource = ds;
+					dataGridView1.DataMember = "tbl_barang";
+				}
+				else {
+					MessageBox.Show("Data tabel sedang kosong");
+				}
+			}
+			catch (Exception ex){
+				MessageBox.Show("Data tidak dapat dimasukkan");
+				MessageBox.Show(ex.ToString());
+			}
+		} 
+		
+		public void Bersihkan(){
+			textBox1.Text="";
+			textBox2.Text="";
+			textBox3.Text="";
+			textBox4.Text="";
+			textBox5.Text="";
+			textBox6.Text="";
+		}
+		
+		
+		
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			co.Open();
+			TampilBarang();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		/* Membuat Event Tombol Simpan */
+		void Button1Click(object sender, EventArgs e)
+		{
+			// Memeriksa apakah kolom text kosong?
+			if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "" )
+			{
+				MessageBox.Show("Mohon isikan dahulu kolom-kolom yang tersedia");
+			}
+			else
+			{
+				/* insert data */
+				try{
+					mycommand.Connection=co;
+					mycommand.CommandText="insert into tbl_barang values('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox3.Text+"','"+textBox4.Text+"','"+textBox5.Text+"','"+textBox6.Text+"')";
+					myadapter.SelectCommand= mycommand;
+					if (mycommand.ExecuteNonQuery()==1){
+						MessageBox.Show("Data berhasil dimasukan","Informasi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+						TampilBarang();
+						Bersihkan();
+					}
+				}
+				catch(Exception ex){
+					MessageBox.Show(ex.ToString());
+				}
+			}
+		}
+		
+		
+		
+		void DataGridView1CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			try{
+				textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+				textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+				textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+				textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+				textBox5.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+				textBox6.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+			}
+			catch(Exception ex){
+				MessageBox.Show(ex.ToString());
+			}
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			/* tombol update data */
+			// Memeriksa apakah kolom text kosong?
+			if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "" )
+			{
+				MessageBox.Show("Mohon isikan dahulu kolom-kolom yang tersedia");
+			}
+			else
+			{
+				/* update data */
+				try{
+					mycommand.Connection=co;
+					mycommand.CommandText="update tbl_barang set KodeBarang='"+textBox1.Text+"', NamaBarang='"+textBox2.Text+"', HargaJual='"+textBox3.Text+"', HargaBeli='"+textBox4.Text+"', JumlahBarang='"+textBox5.Text+"', SatuanBarang='"+textBox6.Text+"' where KodeBarang='"+textBox1.Text+"'";
+					myadapter.SelectCommand= mycommand;
+					if (mycommand.ExecuteNonQuery()==1){
+						MessageBox.Show("Update data berhasil","Informasi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+						TampilBarang();
+						Bersihkan();
+					}
+				}
+				catch(Exception ex){
+					MessageBox.Show(ex.ToString());
+				}
+			}
+		}
+	}
+}
